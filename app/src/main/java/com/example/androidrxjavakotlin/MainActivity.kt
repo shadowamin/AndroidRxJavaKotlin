81ccaf4aaf9cfadaa7ccfdbd84de007eb49b55f7
AMIN_ATITALLAH <atitallah.amin@gmail.com>
2020-06-03 10:48:46 +0200

    first commit

---
+++ app/src/main/java/com/example/androidrxjavakotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.androidrxjavakotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidrxjavakotlin.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
